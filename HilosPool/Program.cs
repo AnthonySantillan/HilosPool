14af81361597027653d4bbd70e81ff66b8dae78e
AnthonySantillan <58825544+AnthonySantillan@users.noreply.github.com>
2021-01-17 13:01:52 -0500

    HilosPool

---
+++ HilosPool/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace HilosPool
+{
+    class Program
+    {
+        const string path = @"C:\Users\hp\Desktop\Tercero\archivoc\";
+        static void Main(string[] args)
+        {
+            for (int i=0; i<=50; i++) 
+            {
+                ThreadPool.QueueUserWorkItem(CrearTxt, i);
+            }
+            while (ThreadPool.PendingWorkItemCount > 0) ;
+
+
+        }
+        static void CrearTxt(object data)
+        {
+            int i = (int)data;
+
+            using (var sw = new StreamWriter(path+i+".txt"))
+            {
+                sw.WriteLine("Hola soy el hilo: "+Thread.CurrentThread.ManagedThreadId);
+            }
+            Console.WriteLine("Hola soy el hilo: " + Thread.CurrentThread.ManagedThreadId);
+
+        }
+    }
+}
